860b90301bc8a993c862497f8328a1788e4814b4
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-02-16 17:49:57 -0300

    second version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 
+import "./App.css";
 
 const API_URL =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
@@ -8,7 +8,7 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
-function App() {
+const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -16,49 +16,39 @@ function App() {
     getMovies(API_URL);
   }, []);
 
-  async function getMovies(url) {
+  const getMovies = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
 
-    setMovies(data.results);
-  }
+    showMovies(data.results);
+  };
 
-  function handleSearch(e) {
+  const showMovies = (movies) => setMovies(movies);
+
+  const getClassByRate = (vote) =>
+    vote >= 8 ? "green" : vote >= 5 ? "orange" : "red";
+
+  const handleOnSubmit = (e) => {
     e.preventDefault();
 
-    if (searchTerm && searchTerm.trim() !== "") {
+    if (searchTerm && searchTerm !== "") {
       getMovies(SEARCH_API + searchTerm);
-
       setSearchTerm("");
     } else {
-      getMovies(API_URL);
+      window.location.reload();
     }
-  }
-
-  function handleChange(e) {
-    setSearchTerm(e.target.value);
-  }
-
-  function getClassByRate(vote) {
-    if (vote >= 8) {
-      return "green";
-    } else if (vote >= 5) {
-      return "orange";
-    } else {
-      return "red";
-    }
-  }
+  };
 
   return (
     <div className="App">
       <header>
-        <form onSubmit={handleSearch}>
+        <form onSubmit={handleOnSubmit}>
           <input
             type="text"
-            value={searchTerm}
-            onChange={handleChange}
             className="search"
             placeholder="Search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
         </form>
       </header>
@@ -68,7 +58,7 @@ function App() {
           const { id, title, poster_path, vote_average, overview } = movie;
 
           return (
-            <div className="movie" key={id}>
+            <div key={id} className="movie">
               <img src={IMG_PATH + poster_path} alt={title} />
               <div className="movie-info">
                 <h3>{title}</h3>
@@ -86,6 +76,6 @@ function App() {
       </main>
     </div>
   );
-}
+};
 
 export default App;
